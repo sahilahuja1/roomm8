17a41ae58d01de23fdae3526bb93a8a1611b37ad
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-28 12:14:16 -0400

    stub code for receiving message

--- app.js
+++ app.js
@@ -17,6 +17,35 @@ app.get('/webhook', function(req, res) {
   }  
 });
 
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
 
 // Start listening for requests
 app.listen((process.env.PORT || 3000));
