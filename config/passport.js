5add0fc39074a2808754b6fdc027803470f4c71d
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-28 17:30:14 -0400

    started adding passport

--- config/passport.js
+++ config/passport.js
@@ -6,29 +6,23 @@ var configAuth = require('./auth');
 
 module.exports = function(passport) {    
     passport.use(
-        new FacebookStrategy({
-            clientID : configAuth.facebookAuth.clientID,
-            clientSecret : configAuth.facebookAuth.clientSecret,
-            callbackURL : configAuth.facebookAuth.callbackURL
-        }),
-        function(token, refreshToken, profile, done) {
-            mongo.user.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-                if (err) {
-                    return done(err);
-                } else if (user) {
-                    return done(null, user);
-                } else {
-                    var newUser = new mongo.user();
-                    newUser.id = profile.id;
-                    newUser.token = token;
-                    newUser.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                    newUser.email = profile.emails[0].value;
-
-                    newUser.save(function(err) {
-                        if (err) throw err;
-                        return done(null, newUser);
-                    });
-                }
-            });
-        });
+        new FacebookStrategy(
+            {
+                clientID : configAuth.facebookAuth.clientID,
+                clientSecret : configAuth.facebookAuth.clientSecret,
+                callbackURL : configAuth.facebookAuth.callbackURL
+            },
+            function(accessToken, refreshToken, profile, cb) {
+                mongo.user.findOrCreate(
+                    { 'id' : profile.id,
+                      'token' : token,
+                       'email' : profile.emails[0].value,
+                       'name' : profile.name.givenName + ' ' + profile.name.familyName },
+                    function (err, user) {
+                        return cb(err, user);
+                    }
+                );
+            }
+        )
+    );
 };
