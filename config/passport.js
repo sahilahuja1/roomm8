65ae3020c655283b53a161d74777b885769e4ca8
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-28 17:16:38 -0400

    started adding passport

--- config/passport.js
+++ config/passport.js
@@ -5,29 +5,30 @@ var mongo = require('../db/mongo');
 var configAuth = require('./auth');
 
 module.exports = function(passport) {    
-    passport.use(new FacebookStrategy({
-        clientID : configAuth.facebookAuth.clientID,
-        clientSecret : configAuth.facebookAuth.clientSecret,
-        callbackURL : configAuth.facebookAuth.callbackURL
-    },
-    function(token, refreshToken, profile, done) {
-        mongo.user.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-            if (err) {
-                return done(err);
-            } else if (user) {
-                return done(null, user);
-            } else {
-                var newUser = new mongo.user();
-                newUser.id = profile.id;
-                newUser.token = token;
-                newUser.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                newUser.email = profile.emails[0].value;
+    passport.use(
+        new FacebookStrategy({
+            clientID : configAuth.facebookAuth.clientID,
+            clientSecret : configAuth.facebookAuth.clientSecret,
+            callbackURL : configAuth.facebookAuth.callbackURL
+        }),
+        function(token, refreshToken, profile, done) {
+            mongo.user.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                if (err) {
+                    return done(err);
+                } else if (user) {
+                    return done(null, user);
+                } else {
+                    var newUser = new mongo.user();
+                    newUser.id = profile.id;
+                    newUser.token = token;
+                    newUser.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                    newUser.email = profile.emails[0].value;
 
-                newUser.save(function(err) {
-                    if (err) throw err;
-                    return done(null, newUser);
-                });
-            }
+                    newUser.save(function(err) {
+                        if (err) throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
         });
-    }));
 };
