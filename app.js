b5c7fb552474cb2dd5bc60b3275b859e97516f8d
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-28 12:07:01 -0400

    updated procfile

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ app.get('/', function (req, res) {
 });
 
 // Start listening for requests
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
-});
-
+app.listen((process.env.PORT || 3000));
 
 module.exports = app;
