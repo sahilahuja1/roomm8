cc4c8d54f71dc6cf8d23d2e7b5bafd6cd4e4ed9d
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-28 15:18:09 -0400

    started adding passport

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebookAuth' : {
+        'clientID'      : '1766931530285001', // your App ID
+        'clientSecret'  : '59a312aff683f66388dc8b455c315a55', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    }
+}
---
+++ config/passport.js
@@ -0,0 +1,49 @@
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+// load up the user model
+var User = require('../app/models/user');
+
+// load the auth variables
+var configAuth = require('./auth');
+
+module.exports = function(passport) {
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+    
+    passport.use(new FacebookStrategy({
+        // pull in our app id and secret from our auth.js file
+        clientID : configAuth.facebookAuth.clientID,
+        clientSecret : configAuth.facebookAuth.clientSecret,
+        callbackURL : configAuth.facebookAuth.callbackURL
+    },
+    function(token, refreshToken, profile, done) {
+        User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+            if (err) {
+                return done(err);
+            } else if (user) {
+                return done(null, user);
+            } else {
+                var newUser = new User();
+                newUser.id = profile.id;
+                newUser.token = token;
+                newUser.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                newUser.email = profile.emails[0].value;
+
+                newUser.save(function(err) {
+                    if (err) throw err;
+                    return done(null, newUser);
+                });
+            }
+        });
+    });
+};
