8b52b6772849356dbef20e31d6e65c98997ffb45
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-28 12:09:05 -0400

    started messenger webhook

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get('/', function (req, res) {
   return res.send('hello world!');
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'my_verification_token') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+
 // Start listening for requests
 app.listen((process.env.PORT || 3000));
 
