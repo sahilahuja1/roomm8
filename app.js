14f0790f37f2baf1da1dc236d7cfc5028cd4aeb6
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-28 12:18:34 -0400

    stub code for receiving message

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.post('/webhook', function (req, res) {
       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
         if (event.message) {
+    	  res.send(event.message);
           receivedMessage(event);
         } else {
           console.log("Webhook received unknown event: ", event);
@@ -43,8 +44,35 @@ app.post('/webhook', function (req, res) {
 });
   
 function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
 }
 
 // Start listening for requests
