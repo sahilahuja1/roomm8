dc00049429c02319a79698fc178efb3c3c144a09
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-28 14:03:50 -0400

    added mlab

--- db/mongo.js
+++ db/mongo.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://sahilahuja:roompass@ds123351.mlab.com:23351/roomm8');
+// NOTE: CHANGE TO USE ENVIRONMENT VARIABLES FOR USERNAME/PASSWORD
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
