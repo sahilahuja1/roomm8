c2b0e37b50af7b4de0a5f5595a6901ca6e3c3964
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-29 11:02:39 -0400

    main page design

--- js/indexPage.js
+++ js/indexPage.js
@@ -2,6 +2,6 @@ var mongo = require('../db/mongo');
 
 mongo.message.find({sender: '1314955871892871' }, function(e, message) {
 	message.forEach(function (msg) {
-		$('#nav').append('<p>', msg.messageText, '</p>');
+		$('#nav').append(msg.messageText);
 	});
 });
