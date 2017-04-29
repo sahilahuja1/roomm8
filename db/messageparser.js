70590cc1833b12ab9c69c31f03cab5b8276da84e
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-29 18:12:25 -0400

    creating room

--- db/messageparser.js
+++ db/messageparser.js
@@ -13,6 +13,7 @@ var identifyUser = function(senderId, PAGE_ACCESS_TOKEN) {
 	mongo.user.findOne({ 'pgid': senderId } , function (err, person) {
 		if (!err) {
 			if (!person) {
+				console.log('1');
 				request({
 			  		method: 'GET',
 					uri: `https://graph.facebook.com/v2.6/${senderId}`,
@@ -22,18 +23,22 @@ var identifyUser = function(senderId, PAGE_ACCESS_TOKEN) {
 					},
 				  	json: true
 				}, function(error, response, body) {
+					console.log('2');
 					if (response.statusCode == 200) {
 						mongo.user.findOne({'name' : {$regex : '.*' + body.first_name + '.*' + body.last_name + '.*'}}, 
 							function (err, person) {
+								console.log('3');
 								person.pgid = senderId;
 								return person.id;
 							}
 						);
 					} else {
+						console.log('4');
 						return 0;
 					}
 				});
 			} else {
+				console.log('5');
 				return person.id;
 			}
 		}
@@ -51,22 +56,25 @@ var parseMessage = function(message, senderId, PAGE_ACCESS_TOKEN, sendMessage) {
  //  });
 
   if (text.includes('create new room')) {
+  	console.log('6');
   	var roomId = mongoose.Types.ObjectId();
+  	console.log('7');
   	mongo.user.findOne({ 'id': id } , function (err, person) {
+  		console.log('8');
   		if (!err && person) {
+  			console.log('9');
   			if (!person.room) {
+  				console.log('10');
   				person.room = roomId;
   				sendMessage(senderId, 'Successfully created new room. Now add friends!');
   			} else {
+  				console.log('11');
   				sendMessage(senderId, 'You are already in a room! Leave that room first.');
   			}
   		}
 	});
 
   }
-
-
-
 
 
 
