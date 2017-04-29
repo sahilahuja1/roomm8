2fe6eb90d60b87890cecbbea87a2090e6165604a
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-29 16:34:03 -0400

    adding message parsing

---
+++ db/messageparser.js
@@ -0,0 +1,23 @@
+var mongo = require('./mongo');
+var passport = require('passport');
+var mongoose = require('mongoose');
+
+
+var parseMessage = function(message, senderId, sendMessage) {
+  var messageDb = new mongo.message({
+	sender: senderID,
+	messageId: message.mid,
+	messageText: message.text
+  });
+
+  messageDb.save(function(err) {
+    if (err) throw err;
+    console.log('Message saved!');
+  });
+
+  if (message.text) {
+  	sendMessage(senderID, message.text);
+  }
+};
+
+module.exports = parseMessage;
