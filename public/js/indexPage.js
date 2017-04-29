1c5f8c0c30af5489cf6f4171f14cf28d810bc760
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-29 16:01:50 -0400

    added public folder

--- public/js/indexPage.js
+++ public/js/indexPage.js
@@ -1,6 +1,7 @@
 var mongo = require('../../db/mongo');
 
-mongo.message.find({sender: '1314955871892871' }, function(e, message) {
+mongo.message.find({sender: '1314955871892871'}, function(e, message) {
+	console.log('here');
 	message.forEach(function (msg) {
 		$('#nav').append(msg.messageText);
 		console.log(msg.messageText);
