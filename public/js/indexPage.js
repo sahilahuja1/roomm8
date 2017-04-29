8fa87b37cfd09abc3cf0eab6583a30972fbb2ce1
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-29 16:22:16 -0400

    adding mongo data to webpage

--- public/js/indexPage.js
+++
@@ -1,8 +0,0 @@
-
-mongo.message.find({sender: '1314955871892871'}, function(e, message) {
-	console.log('here');
-	message.forEach(function (msg) {
-		$('#nav').append(msg.messageText);
-		console.log(msg.messageText);
-	});
-});
