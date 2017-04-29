62719f0cc6052e7adef83fb8c954e7a1be751b44
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-29 16:45:53 -0400

    creating room

--- db/messageparser.js
+++ db/messageparser.js
@@ -2,13 +2,36 @@ var mongo = require('./mongo');
 var passport = require('passport');
 var mongoose = require('mongoose');
 
+/*
+create new room
+join room Sahil
+leave room
+
+*/
+
 
 var parseMessage = function(message, senderId, sendMessage) {
+  var text = message.text.toLowerCase();
   var messageDb = new mongo.message({
 	sender: senderId,
 	messageId: message.mid,
 	messageText: message.text
   });
+
+  if (text.includes('create new room')) {
+  	var roomId = var id = mongoose.Types.ObjectId();
+  	mongo.user.findOne({ 'id': senderId } , function (err, person) {
+  		if (!err) {
+  			person.room = roomId;
+  		}
+	});
+
+  }
+
+
+
+
+
 
   messageDb.save(function(err) {
     if (err) throw err;
