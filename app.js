059b7246354bc89d51f3a2ace75a324223c04a5e
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-29 15:15:26 -0400

    debugging login

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/', loginRouter);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/auth/facebook', passport.authenticate('facebook',{ scope: 'read_stream'}));
+app.get('/auth/facebook', passport.authenticate('facebook',{ scope: ['user_friends']}));
 
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }),
