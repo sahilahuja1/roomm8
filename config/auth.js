5f6dcca4a4de7c8def0277bdea59b78535e5bc7c
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-29 14:04:03 -0400

    starting login

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1766931530285001', // your App ID
         'clientSecret'  : '59a312aff683f66388dc8b455c315a55', // your App Secret
-        'callbackURL'   : 'http://roomm8app.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : 'https://roomm8app.herokuapp.com/auth/facebook/callback'
     }
 }
