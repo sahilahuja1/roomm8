1d93dd96e5d8fcf2a5a760e01067429bafe56844
sahilahuja1 <sahilahu@seas.upenn.edu>
2017-04-30 03:26:09 -0400

    get chores

--- db/messageparser.js
+++ db/messageparser.js
@@ -114,7 +114,7 @@ var parseMessage = function(message, id, senderId, PAGE_ACCESS_TOKEN, sendMessag
     if (text.includes('get chores')) {
       mongo.chore.findOne({'room' : person.room},
         function (err, chore) {
-          console.log(chore);
+          console.log(chore.chores.toString());
           // sendMessage(senderId, chore.chores);
         }
       );
